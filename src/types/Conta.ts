25ba230f6ec5e456efa575af7a36c159562565e8
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2023-08-07 17:21:43 -0300

    Criação dos arquivos: Conta.ts e Armazenador.ts

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,11 +1,12 @@
-import { GrupoTransacao } from "./GrupoTransacao";
-import { TipoTransacao } from "./TipoTransacao";
-import { Transacao } from "./Transacao";
+import { Armazenador } from "./Armazenador.js";
+import { GrupoTransacao } from "./GrupoTransacao.js";
+import { TipoTransacao } from "./TipoTransacao.js";
+import { Transacao } from "./Transacao.js";
 
 export class Conta {
-    nome: string;
-    saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
-    transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: any) => {
+    protected nome: string;
+    protected saldo: number = Armazenador.obter("saldo") || 0;
+    private transacoes: Transacao[] = Armazenador.obter(("transacoes"), (key: string, value: any) => {
         if (key === "data") {
             return new Date(value);
         }
@@ -16,6 +17,10 @@ export class Conta {
         this.nome = nome;
     }
 
+    public getTitular() {
+        return this.nome;
+    }
+    
     getGruposTransacoes(): GrupoTransacao[] {
         const gruposTransacoes: GrupoTransacao[] = [];
         const listaTransacoes: Transacao[] = structuredClone(this.transacoes);
@@ -59,7 +64,7 @@ export class Conta {
 
         this.transacoes.push(novaTransacao);
         console.log(this.getGruposTransacoes());
-        localStorage.setItem("transacoes", JSON.stringify(this.transacoes));
+        Armazenador.salvar("transacoes", JSON.stringify(this.transacoes));
     }
 
     debitar(valor: number): void {
@@ -71,7 +76,7 @@ export class Conta {
         }
     
         this.saldo -= valor;
-        localStorage.setItem("saldo", this.saldo.toString());
+        Armazenador.salvar("saldo", this.saldo.toString());
     }
    
     depositar(valor: number): void {
@@ -80,11 +85,12 @@ export class Conta {
         }
     
         this.saldo += valor;
-        localStorage.setItem("saldo", this.saldo.toString());
+        Armazenador.salvar("saldo", this.saldo.toString());
     }
 }
 
 
 const conta = new Conta("Joana da Silva Olveira");
+//console.log(" ********* ",conta.transacoes())
 
 export default conta;
